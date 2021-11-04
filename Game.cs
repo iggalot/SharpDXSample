31ee8e377d967387992494164d88a6255e235943
Jim <iggalot.iggy@gmail.com>
2021-11-04 15:58:09 -0500

    Functional DirectX11 sample.

---
+++ Game.cs
@@ -0,0 +1,158 @@
+﻿using SharpDX.D3DCompiler;
+using SharpDX;
+using SharpDX.Direct3D;
+using SharpDX.DXGI;
+using SharpDX.Windows;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using D3D11 = SharpDX.Direct3D11;
+
+namespace SharpDXSample
+{
+    public class Game : IDisposable
+    {
+        private SharpDX.Mathematics.Interop.RawViewportF viewport;
+        private D3D11.VertexShader vertexShader;
+        private D3D11.PixelShader pixelShader;
+
+        private D3D11.InputElement[] inputElements = new D3D11.InputElement[] 
+        { 
+            new D3D11.InputElement("POSITION", 0, Format.R32G32B32_Float, 0, 0, D3D11.InputClassification.PerVertexData, 0),
+            new D3D11.InputElement("COLOR", 0, Format.R32G32B32A32_Float, 12, 0, D3D11.InputClassification.PerVertexData, 0)
+        };
+        private ShaderSignature inputSignature;
+        private D3D11.InputLayout inputLayout;
+
+        private VertexPositionColor[] vertices = new VertexPositionColor[]
+        {
+            new VertexPositionColor(new Vector3(-0.5f, 0.5f, 0.0f), SharpDX.Color.Red),
+            new VertexPositionColor(new Vector3(0.5f, 0.5f, 0.0f), SharpDX.Color.Blue),
+            new VertexPositionColor(new Vector3(0.0f, -0.5f, 0.0f), SharpDX.Color.Green)
+        };
+        private D3D11.Buffer triangleVertexBuffer;
+
+        private D3D11.Device d3dDevice;
+        private D3D11.DeviceContext d3dDeviceContext;
+        private SwapChain swapChain;
+
+        private D3D11.RenderTargetView renderTargetView;
+
+        private RenderForm renderForm;
+
+        private const int Width = 1280;
+        private const int Height = 720;
+
+        public Game()
+        {
+            renderForm = new RenderForm("My first SharpDX game");
+            renderForm.ClientSize = new Size(Width, Height);
+            renderForm.AllowUserResizing = false;
+
+            InitializeDeviceResources();
+            InitializeShaders();
+            InitializeTriangle();
+
+        }
+
+        public void Run()
+        {
+            // Start the render loop
+            RenderLoop.Run(renderForm, RenderCallback);
+        }
+
+        private void RenderCallback()
+        {
+            Draw();
+        }
+
+        public void Dispose()
+        {
+            inputLayout.Dispose();
+            inputSignature.Dispose();
+            triangleVertexBuffer.Dispose();
+            vertexShader.Dispose();
+            pixelShader.Dispose();
+            renderTargetView.Dispose();
+            swapChain.Dispose();
+            d3dDevice.Dispose();
+            d3dDeviceContext.Dispose();
+            renderForm.Dispose();
+        }
+
+        private void InitializeDeviceResources()
+        {
+            ModeDescription backBufferDesc = new ModeDescription(Width, Height, new Rational(60, 1), Format.R8G8B8A8_UNorm);
+
+            // Descriptor for the swap chain
+            SwapChainDescription swapChainDesc = new SwapChainDescription()
+            {
+                ModeDescription = backBufferDesc,
+                SampleDescription = new SampleDescription(1, 0),
+                Usage = Usage.RenderTargetOutput,
+                BufferCount = 1,
+                OutputHandle = renderForm.Handle,
+                IsWindowed = true
+            };
+            
+            D3D11.Device.CreateWithSwapChain(DriverType.Hardware, D3D11.DeviceCreationFlags.None, swapChainDesc, out d3dDevice, out swapChain);
+            d3dDeviceContext = d3dDevice.ImmediateContext;
+
+            using(D3D11.Texture2D backBuffer = swapChain.GetBackBuffer<D3D11.Texture2D>(0))
+            {
+                renderTargetView = new D3D11.RenderTargetView(d3dDevice, backBuffer);
+            }
+
+            // Set viewport
+            viewport = new SharpDX.Viewport(0,0,Width,Height);
+
+            d3dDeviceContext.Rasterizer.SetViewport(viewport);
+        }
+
+        private void Draw()
+        {
+            // Set back buffer as current render target view
+            d3dDeviceContext.OutputMerger.SetRenderTargets(renderTargetView);
+
+            // Clear the screen -- Blue color RGBA(32, 103, 178, 255)
+            d3dDeviceContext.ClearRenderTargetView(renderTargetView, new SharpDX.Color(32, 103, 178));
+
+            d3dDeviceContext.InputAssembler.SetVertexBuffers(0, new D3D11.VertexBufferBinding(triangleVertexBuffer, Utilities.SizeOf<VertexPositionColor>(), 0));
+            d3dDeviceContext.Draw(vertices.Count(), 0);
+
+            // Swap the front and back buffer
+            swapChain.Present(1, PresentFlags.None);
+        }
+
+        private void InitializeTriangle()
+        {
+            triangleVertexBuffer = D3D11.Buffer.Create<VertexPositionColor>(d3dDevice, D3D11.BindFlags.VertexBuffer, vertices);
+        }
+
+        private void InitializeShaders()
+        {
+            using (var vertexShaderByteCode = ShaderBytecode.CompileFromFile("vertexShader.hlsl", "main", "vs_4_0", ShaderFlags.Debug))
+            {
+                inputSignature = ShaderSignature.GetInputSignature(vertexShaderByteCode);
+                vertexShader = new D3D11.VertexShader(d3dDevice, vertexShaderByteCode);
+            }
+            using (var pixelShaderByteCode = ShaderBytecode.CompileFromFile("pixelShader.hlsl", "main", "ps_4_0", ShaderFlags.Debug))
+            {
+                pixelShader = new D3D11.PixelShader(d3dDevice, pixelShaderByteCode);
+            }
+
+            // Set as current vertex and pixel shaders
+            d3dDeviceContext.VertexShader.Set(vertexShader);
+            d3dDeviceContext.PixelShader.Set(pixelShader);
+
+            d3dDeviceContext.InputAssembler.PrimitiveTopology = PrimitiveTopology.TriangleList;
+            inputLayout = new D3D11.InputLayout(d3dDevice, inputSignature, inputElements);
+            d3dDeviceContext.InputAssembler.InputLayout = inputLayout;
+
+
+        }
+    }
+}
